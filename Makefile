3fd060f0f06116e8d5d5d1be48ea1f886d6b3e09
Musagen12 <colonelmkar@outlook.com>
2026-04-18 15:08:55 +0300

    First kernel module

---
+++ Makefile
@@ -0,0 +1,9 @@
+obj-m += driver.o
+
+KDIR := /home/colonel/kernel-test-bed/linux-6.19
+
+all:
+	make -C $(KDIR) M=$(PWD) modules
+
+clean:
+	make -C $(KDIR) M=$(PWD) clean
---
+++ driver.c
@@ -0,0 +1,18 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+MODULE_AUTHOR("Musagen12");
+MODULE_DESCRIPTION("This is a test driver for local experimentation");
+MODULE_LICENSE("GPL");
+
+static int __init custom_init(void) {
+	printk(KERN_INFO "Hello Linux World. Driver loaded.");
+	return 0;
+}
+
+static void __exit custom_exit(void) {
+	printk(KERN_INFO "Goodbye.");
+}
+
+module_init(custom_init);
+module_exit(custom_exit);

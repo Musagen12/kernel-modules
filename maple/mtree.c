604e6a3768ffedcbb1612d845b1cc492b2c69954
Musagen12 <colonelmkar@outlook.com>
2026-07-09 22:26:35 +0200

    Added an entry into the maple tree

--- maple/mtree.c
+++ maple/mtree.c
@@ -7,7 +7,10 @@
 struct maple_tree *tree;
 
 static int __init create_tree(void) {
-	printk(KERN_INFO "Hello kernel.");
+	int err;
+	int entry = 567886761;
+
+	printk(KERN_INFO "Welcome to maple tree hacking.");
 
 	// Allocating the memory
 	tree = kmalloc(sizeof(tree), GFP_NOWAIT);
@@ -28,11 +31,27 @@ static int __init create_tree(void) {
 	printk(KERN_INFO "The root pointer address is %p.", &root);
 	printk(KERN_INFO "The root pointer is %p.", root);
 
+	err = mtree_insert_range(tree, 0, 0, &entry, GFP_NOWAIT);
+
+	if (err){
+		printk(KERN_ALERT "An error %d occured.", err);
+		return -1;
+	}
+
+	printk(KERN_INFO "Stored an entry %d.", entry);
+
+	void * post_root = rcu_dereference(tree->ma_root);
+
+	// After storing an entry
+	printk(KERN_INFO "The root pointer address is %p.", &post_root);
+	printk(KERN_INFO "The root pointer is %p.", post_root);
+
 	return 0;
 }
 
 static void __exit cleanup(void) {
 	printk(KERN_INFO "Freeing used memory.");
+	// You need to free any allocated memory to avoid
 	kfree(tree);
 	printk(KERN_INFO "Goodbye kernel.");
 }
